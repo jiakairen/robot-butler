3a6f4a38414db10e844836c540ed73cfbc71b09d
Jason Luo <jasonluo1902@gmail.com>
2022-12-08 14:29:55 +1100

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcU7aVU1d1OHQjim_0kJNxLFg4VRP8cu8",
+  authDomain: "robot-butler-cea98.firebaseapp.com",
+  projectId: "robot-butler-cea98",
+  storageBucket: "robot-butler-cea98.appspot.com",
+  messagingSenderId: "655628721375",
+  appId: "1:655628721375:web:07a92896d59f387ad29d6a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
